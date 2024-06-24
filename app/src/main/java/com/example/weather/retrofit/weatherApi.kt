ea82b3504c97fd123a5a126a07c40916ee0fbb47
sergeyboykov <sutemi67@gmail.com>
2024-06-24 18:25:56 +0300

    patch

--- app/src/main/java/com/example/weather/retrofit/weatherApi.kt
+++ app/src/main/java/com/example/weather/retrofit/weatherApi.kt
@@ -1,7 +1,21 @@
 package com.example.weather.retrofit
 
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 //посылаем POST-запрос на авторизацию
 class ForecaAuthRequest(val user: String, val password: String)
 
 //опишем класс для ответа, получаем токен для последующих запросов
-class ForecaAuthResponse(@SerializedName("access_token") val token: String)
+class ForecaAuthResponse(@SerializedName("access_token") val token: String) {
+    annotation class SerializedName(val value: String)
+}
+
+//опишем запрос в интерфейсе
+interface ForecaApi {
+
+    @POST("/authorize/token?expire_hours=-1")
+    fun authenticate(@Body request: ForecaAuthRequest): Call<ForecaAuthResponse>
+}
+
